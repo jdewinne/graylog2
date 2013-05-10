a42c71683d7380711a61a29eabcccf08867b856a
rchrd <richard.pot@me.com>
2013-05-10 14:38:30 +0200

    Added all modules to node definition

--- manifests/default.pp
+++ manifests/default.pp
@@ -4,9 +4,12 @@ node default {
     enable => false,
   }
 
+  notice("Please be patient, this Puppet run can take up to 15 minutes to complete...")
   package { 'java-1.7.0-openjdk': ensure => present } ->
   class   { 'mongodb': }                              ->
   class   { 'elasticsearch': cluster => 'graylog2' }  ->
-  class   { 'graylog2': }
+  class   { 'rvm': version => '1.9.3' }               ->
+  class   { 'graylog2::server': }                     ->
+  class   { 'graylog2::web': ruby_version => '1.9.3' }
 }
 
